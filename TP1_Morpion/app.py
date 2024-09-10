b4fb3ab27a2a64a039e22c8feed331a9c04dc95b
GALLIC-A <axel.gallic@orange.fr>
2024-09-10 11:29:33 +0200

    ça fonctionne ouaaais

--- TP1_Morpion/app.py
+++ TP1_Morpion/app.py
@@ -1,4 +1,5 @@
 from morpion import TicTacToe
+import random
 
 
 game = TicTacToe()
@@ -10,3 +11,26 @@ game.play(5)
 
 print(game)
 
+
+def opponent_random(game: TicTacToe):
+    moves = game.allowed_moves()
+    game.play(moves[random.randint(0, moves.size)])
+
+
+def opponent_next(game: TicTacToe):
+    for move in range(1, 10):
+        if game.play(move):
+            if game.has_winner() == game.currentPlayer:
+                return
+            game.undo_move(move)
+
+    for move in range(1, 10):
+        if game.is_move_allowed(move):
+            game.play(move)
+            if game.check_winner() == game.opponent_player:
+                game.undo_move(move)
+                game.play(move)
+                return
+            game.undo_move(move)
+
+    opponent_random(game)
--- TP1_Morpion/morpion.py
+++ TP1_Morpion/morpion.py
@@ -1,34 +1,41 @@
 class TicTacToe:
   def __init__(self):
-    board = [
+    board = self.create_board()
+    self.currentPlayer = True  # True : X; False : O
+    self._board = board
+
+# =================================================================
+# =================================================================
+  def create_board() -> list[list[str]]:
+    return [
         [
             None for _ in range(3)
         ] for _ in range(3)
     ]
-
-    self._player = True  # True : X; False : O
-
-    self._board = board
-
+    
+  def reset(self):
+    self._board = self.create_board()
+    self.currentPlayer = True
 # =================================================================
-
-  def play(self, number: int):
+# =================================================================
+  def play(self, number: int) -> bool:
     if number < 1 or number > 9:
       print("La case est hors de la grille")
-      return
+      return False
 
     row = (number - 1) // 3  # Ligne
     col = (number - 1) % 3   # Colonne
 
     if self._board[row][col] is not None:
       print("Cette case est déjà occupée")
+      return False
 
     else:
-      self._board[row][col] = self._player
-      self._player = not self._player
-
+      self._board[row][col] = self.currentPlayer
+      self.currentPlayer = not self.currentPlayer
+      return False
 # =================================================================
-
+# =================================================================
   def __str__(self):
       return '\n'.join(
           [
@@ -38,17 +45,57 @@ class TicTacToe:
               for row in self._board
           ]
       )
-
+# =================================================================
+# =================================================================
   def display_value(self, cell):
     if cell is None:
         return '.'
     return 'X' if cell else 'O'
+# =================================================================
+# =================================================================
+  def undo(self, row: int, column: int):
+    self._board[row][column] = None
+# =================================================================
+# =================================================================
+  def has_winner(self) -> bool:
+    last_player = self.currentPlayer
 
-  def undo(row: int, column: int):
-    self._board[row][column] = ""
-
-  def has_winner():
+    for row in self._board:
+      if row == [last_player, last_player, last_player]: 
+        return True
+    
+    for col in range(3):
+      if(self._board[0][col] == last_player and self._board[1][col] == last_player and self._board[2][col] == last_player):
+        return True
+    
+    if(self._board[0][0] == last_player and self._board[1][1] == last_player and self._board[2][2] == last_player):
+      return True
+    
+    if(self._board[0][2] == last_player and self._board[1][1] == last_player and self._board[2][0]):
+      return True
+    
+    return False
+# =================================================================
+# =================================================================
+  def is_draw(self):
+    if self.has_winner():
+      return False
+    
+    # Vérifie si existent encore des cases avec None
+    for row in self._board:
+      if None in row:
+        return False
+    
     return True
+# =================================================================
+# =================================================================
+  def allowed_moves(self):
+    moves = []
+    
+    for i in range(3):
+      for j in range(3):
+        if self._board[i][j] is None:
+          move_number = i * 3 + j + 1
+          moves.append(move_number)
 
-  def is_draw():
-    print("Match nul")
+    return moves
