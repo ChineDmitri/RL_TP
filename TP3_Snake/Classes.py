c14c915692652ac841448252a5605c5e0c8d0782
ChineDmitri <mr.dima11@gmail.com>
2024-10-08 12:59:08 +0200

    Greedy player and save statee

--- TP3_Snake/Classes.py
+++ TP3_Snake/Classes.py
@@ -48,3 +48,101 @@ class ShortSnake:
             grid_str += '\n'
         grid_str += f'Score: {self.score}\n'
         return grid_str
+
+    def get_reward(self):
+        if self.snake_pos == self.fruit_pos:
+            return 10  # Récompense pour avoir mangé le fruit
+        elif (self.snake_pos[0] < 0 or self.snake_pos[0] >= self.grid_size or
+              self.snake_pos[1] < 0 or self.snake_pos[1] >= self.grid_size):
+            return -10  # Pénalité pour collision ou sortie
+        else:
+            return 0  # Aucune récompense si rien de spécial ne se passe
+
+    def step(self, action):
+        actions = {
+            'up': [-1, 0],
+            'down': [1, 0],
+            'left': [0, -1],
+            'right': [0, 1]
+        }
+        if action in actions:
+            self.move(actions[action])
+
+        state = (self.snake_pos, self.fruit_pos)
+        reward = self.get_reward()
+        done = reward == -10
+
+        return state, reward, done
+
+    def save_state(self):
+        return (tuple(self.snake_pos), tuple(self.fruit_pos), tuple(self.snake_dir), self.score)
+
+    # Nouvelle méthode pour restaurer un état sauvegardé
+    def restore_state(self, state):
+        self.snake_pos,
+        self.fruit_pos,
+        self.snake_dir,
+        self.score = map(list, state)
+
+
+################################################################
+################################################################
+################################################################
+
+
+class GreedyPlayer:
+    def __init__(self, actions, eps):
+        """
+        actions: liste des actions possibles (ex: ['up', 'down', 'left', 'right'])
+        eps: taux d'exploration
+        """
+        self.actions = actions
+        # Valeurs initiales des actions
+        self.action_values = {action: 0 for action in actions}
+        # Compteur d'évaluation pour chaque action
+        self.eval_count = {action: 0 for action in actions}
+        self.eps = eps  # Taux d'exploration
+
+    def get_action(self):
+        """
+        Choisir une action soit aléatoire (exploration), soit en suivant la meilleure valeur d'action (exploitation)
+        """
+        rand_value = random.random()
+        explore = rand_value < self.eps  # Si aléatoire est inférieur à epsilon, on explore
+
+        if explore:
+            return self._random_action()  # Choisir une action aléatoire
+        else:
+            # Choisir la meilleure action selon les valeurs d'actions
+            return self._greedy_action()
+
+    def _greedy_action(self):
+        """
+        Choisir l'action avec la meilleure valeur
+        """
+        max_value = max(self.action_values.values())
+
+        # Trouver les actions ayant la valeur maximale
+        best_actions = [action for action,
+                        value in self.action_values.items() if value == max_value]
+
+        # Si plusieurs actions ont la même valeur maximale, en choisir une aléatoirement
+        return random.choice(best_actions)
+
+    def _random_action(self):
+        """
+        Choisir une action aléatoire parmi les actions possibles
+        """
+        return random.choice(self.actions)
+
+    def reward(self, action, reward):
+        """
+        Mettre à jour la valeur de l'action choisie avec la récompense obtenue
+        action: l'action pour laquelle on reçoit la récompense
+        reward: la valeur de la récompense
+        """
+        self.eval_count[action] += 1  # Incrémenter le nombre de fois que cette action a été choisie
+
+        # Mise à jour de la moyenne des valeurs pour cette action
+        self.action_values[action] += (reward -
+                                       self.action_values[action]) / self.eval_count[action]
--- TP3_Snake/app.py
+++ TP3_Snake/app.py
@@ -1,11 +1,30 @@
+import random
+import time
+
 from Classes import ShortSnake
 
-# Rest of the code...
 
-snake = [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1)]
+# États (x snake,y snake,x fruit,y fruit)
+# x snake,y snake -> sont les coordonnées du serpent.
+# x fruit,y fruit -> sont les coordonnées du fruit (pomme).
+# Boucle principale pour exécuter le jeu avec MDP
+def main():
+    mdp_snake = ShortSnake(5)  # Grille de taille 5x5
 
-# Create an instance of ShortSnake with the original snake
-shortened_snake = ShortSnake(5)
+    # Actions possibles
+    actions = ['up', 'down', 'left', 'right']
 
-# Now, 'shortened_snake.shortened_snake' contains the shortened snake
-print(shortened_snake)  # Output: [(1, 1), (2, 1), (3, 1)]
+    while True:
+        print(mdp_snake)
+        # Choisir une action aléatoire ou manuelle
+        action = random.choice(actions)  # Action aléatoire
+        state, reward, done = mdp_snake.step(action)  # Appliquer l'action et obtenir le nouvel état
+
+        print(f'Action: {action}, Reward: {reward}, State: {state}\n')
+        time.sleep(0.1)  # Pause de 0.5 secondes entre chaque tour
+        if done:
+            print("Game Over!")
+            break
+
+if __name__ == "__main__":
+    main()
