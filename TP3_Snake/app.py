d7bb03a4799135891580dc8bd5f5a4ff97addc81
ChineDmitri <mr.dima11@gmail.com>
2024-10-08 11:16:19 +0200

    Super test :D

---
+++ TP3_Snake/app.py
@@ -0,0 +1,11 @@
+from Classes import ShortSnake
+
+# Rest of the code...
+
+snake = [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1)]
+
+# Create an instance of ShortSnake with the original snake
+shortened_snake = ShortSnake(5)
+
+# Now, 'shortened_snake.shortened_snake' contains the shortened snake
+print(shortened_snake)  # Output: [(1, 1), (2, 1), (3, 1)]
