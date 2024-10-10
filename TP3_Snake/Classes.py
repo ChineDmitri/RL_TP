2bcefb591e5fc53f8c8f523b2f8e0609f0bfaa0f
ChineDmitri <mr.dima11@gmail.com>
2024-10-10 10:55:54 +0200

    Add greedy

--- TP3_Snake/Classes.py
+++ TP3_Snake/Classes.py
@@ -4,15 +4,18 @@ import random
 class ShortSnake:
     def __init__(self, grid_size):
         self.grid_size = grid_size
+        self.reset()
+
+    def reset(self):
         self.grid = [
-            [' ' for _ in range(grid_size)]
-            for _ in range(grid_size)
+            [' ' for _ in range(self.grid_size)]
+            for _ in range(self.grid_size)
         ]
-        self.snake_pos = [grid_size // 2, grid_size // 2]
-        # init direction à droite ->
-        self.snake_dir = [0, 1]
+        self.snake_pos = [self.grid_size // 2, self.grid_size // 2]
+        self.snake_dir = [0, 1]  # init direction à droite ->
         self.fruit_pos = self._get_random_fruit_pos()
         self.score = 0
+        return self.get_state()  # Retourne l'état initial
 
     def _get_random_fruit_pos(self):
         return [random.randint(0, self.grid_size - 1), random.randint(0, self.grid_size - 1)]
@@ -68,21 +71,113 @@ class ShortSnake:
         if action in actions:
             self.move(actions[action])
 
-        state = (self.snake_pos, self.fruit_pos)
+        state = self.get_state()
         reward = self.get_reward()
         done = reward == -10
 
         return state, reward, done
 
+    def get_state(self):
+        return (tuple(self.snake_pos), tuple(self.fruit_pos))
+
     def save_state(self):
         return (tuple(self.snake_pos), tuple(self.fruit_pos), tuple(self.snake_dir), self.score)
 
-    # Nouvelle méthode pour restaurer un état sauvegardé
     def restore_state(self, state):
-        self.snake_pos,
-        self.fruit_pos,
-        self.snake_dir,
-        self.score = map(list, state)
+        self.snake_pos, self.fruit_pos, self.snake_dir, self.score = map(list, state[:3])
+        
+# class ShortSnake:
+#     def __init__(self, grid_size):
+#         self.grid_size = grid_size
+#         self.grid = [
+#             [' ' for _ in range(grid_size)]
+#             for _ in range(grid_size)
+#         ]
+#         self.snake_pos = [grid_size // 2, grid_size // 2]
+#         # init direction à droite ->
+#         self.snake_dir = [0, 1]
+#         self.fruit_pos = self._get_random_fruit_pos()
+#         self.score = 0
+
+#     def _get_random_fruit_pos(self):
+#         return [random.randint(0, self.grid_size - 1), random.randint(0, self.grid_size - 1)]
+
+#     def move(self, direction=None):
+#         if direction is not None:
+#             self.snake_dir = direction
+#         new_head = [
+#             (self.snake_pos[0] + self.snake_dir[0]) % self.grid_size,
+#             (self.snake_pos[1] + self.snake_dir[1]) % self.grid_size
+#         ]
+#         if new_head == self.fruit_pos:
+#             self.score += 10
+#             self.fruit_pos = self._get_random_fruit_pos()
+#         elif (new_head[0] < 0
+#               or new_head[0] >= self.grid_size
+#               or new_head[1] < 0
+#               or new_head[1] >= self.grid_size):
+#             self.score -= 10
+
+#         self.snake_pos = new_head
+
+#     def __str__(self):
+#         grid_str = ''
+#         for i in range(self.grid_size):
+#             for j in range(self.grid_size):
+#                 if [i, j] == self.snake_pos:
+#                     grid_str += '🐍 '
+#                 elif [i, j] == self.fruit_pos:
+#                     grid_str += '🍏 '
+#                 else:
+#                     grid_str += ' . '
+#             grid_str += '\n'
+#         grid_str += f'Score: {self.score}\n'
+#         return grid_str
+
+#     def get_reward(self):
+#         if self.snake_pos == self.fruit_pos:
+#             return 10  # Récompense pour avoir mangé le fruit
+#         elif (self.snake_pos[0] < 0 or self.snake_pos[0] >= self.grid_size or
+#               self.snake_pos[1] < 0 or self.snake_pos[1] >= self.grid_size):
+#             return -10  # Pénalité pour collision ou sortie
+#         else:
+#             return 0  # Aucune récompense si rien de spécial ne se passe
+
+#     def step(self, action):
+#         actions = {
+#             'up': [-1, 0],
+#             'down': [1, 0],
+#             'left': [0, -1],
+#             'right': [0, 1]
+#         }
+#         if action in actions:
+#             self.move(actions[action])
+
+#         state = (self.snake_pos, self.fruit_pos)
+#         reward = self.get_reward()
+#         done = reward == -10
+
+#         return state, reward, done
+
+#     def save_state(self):
+#         return (tuple(self.snake_pos), tuple(self.fruit_pos), tuple(self.snake_dir), self.score)
+
+#     def get_state(self):
+#         return (tuple(self.snake_pos), tuple(self.fruit_pos))
+
+#     def restore_state(self, state):
+#         self.snake_pos, self.fruit_pos, self.snake_dir, self.score = map(list, state[:3]) + [state[3]]
+
+#     def reset(self):
+#         self.grid = [
+#             [' ' for _ in range(self.grid_size)]
+#             for _ in range(self.grid_size)
+#         ]
+#         self.snake_pos = [self.grid_size // 2, self.grid_size // 2]
+#         self.snake_dir = [0, 1]  # init direction à droite ->
+#         self.fruit_pos = self._get_random_fruit_pos()
+#         self.score = 0
+#         return self.get_state()  # Retourne l'état initial
 
 
 ################################################################
@@ -91,58 +186,58 @@ class ShortSnake:
 
 
 class GreedyPlayer:
-    def __init__(self, actions, eps):
+    def __init__(self, actions, eps=0.1, alpha=0.1, gamma=0.9):
         """
         actions: liste des actions possibles (ex: ['up', 'down', 'left', 'right'])
         eps: taux d'exploration
+        alpha: taux d'apprentissage
+        gamma: facteur de réduction pour les récompenses futures
         """
         self.actions = actions
-        # Valeurs initiales des actions
-        self.action_values = {action: 0 for action in actions}
-        # Compteur d'évaluation pour chaque action
-        self.eval_count = {action: 0 for action in actions}
-        self.eps = eps  # Taux d'exploration
+        self.eps = eps
+        self.alpha = alpha
+        self.gamma = gamma
+        self.q_values = {}  # Table Q pour stocker les valeurs d'état-action
 
-    def get_action(self):
+    def get_action(self, state):
         """
-        Choisir une action soit aléatoire (exploration), soit en suivant la meilleure valeur d'action (exploitation)
+        Choisir une action basée sur l'état actuel
         """
-        rand_value = random.random()
-        explore = rand_value < self.eps  # Si aléatoire est inférieur à epsilon, on explore
-
-        if explore:
-            return self._random_action()  # Choisir une action aléatoire
+        if random.random() < self.eps:
+            return self._random_action()
         else:
-            # Choisir la meilleure action selon les valeurs d'actions
-            return self._greedy_action()
+            return self._greedy_action(state)
 
-    def _greedy_action(self):
+    def _greedy_action(self, state):
         """
-        Choisir l'action avec la meilleure valeur
+        Choisir l'action avec la meilleure valeur Q pour l'état donné
         """
-        max_value = max(self.action_values.values())
-
-        # Trouver les actions ayant la valeur maximale
-        best_actions = [action for action,
-                        value in self.action_values.items() if value == max_value]
-
-        # Si plusieurs actions ont la même valeur maximale, en choisir une aléatoirement
+        if state not in self.q_values:
+            return self._random_action()
+        
+        q_values = self.q_values[state]
+        max_q = max(q_values.values())
+        best_actions = [action for action, value in q_values.items() if value == max_q]
         return random.choice(best_actions)
 
     def _random_action(self):
         """
-        Choisir une action aléatoire parmi les actions possibles
+        Choisir une action aléatoire
         """
         return random.choice(self.actions)
 
-    def reward(self, action, reward):
+    def update(self, state, action, reward, next_state):
         """
-        Mettre à jour la valeur de l'action choisie avec la récompense obtenue
-        action: l'action pour laquelle on reçoit la récompense
-        reward: la valeur de la récompense
+        Mettre à jour la valeur Q pour un couple état-action
         """
-        self.eval_count[action] += 1  # Incrémenter le nombre de fois que cette action a été choisie
-
-        # Mise à jour de la moyenne des valeurs pour cette action
-        self.action_values[action] += (reward -
-                                       self.action_values[action]) / self.eval_count[action]
+        if state not in self.q_values:
+            self.q_values[state] = {a: 0 for a in self.actions}
+        
+        if next_state not in self.q_values:
+            self.q_values[next_state] = {a: 0 for a in self.actions}
+        
+        current_q = self.q_values[state][action]
+        max_next_q = max(self.q_values[next_state].values())
+        
+        new_q = current_q + self.alpha * (reward + self.gamma * max_next_q - current_q)
+        self.q_values[state][action] = new_q
--- TP3_Snake/app.py
+++ TP3_Snake/app.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-from Classes import ShortSnake
+from Classes import ShortSnake, GreedyPlayer
 
 
 # États (x snake,y snake,x fruit,y fruit)
@@ -9,22 +9,27 @@ from Classes import ShortSnake
 # x fruit,y fruit -> sont les coordonnées du fruit (pomme).
 # Boucle principale pour exécuter le jeu avec MDP
 def main():
-    mdp_snake = ShortSnake(5)  # Grille de taille 5x5
-
-    # Actions possibles
     actions = ['up', 'down', 'left', 'right']
-
-    while True:
-        print(mdp_snake)
-        # Choisir une action aléatoire ou manuelle
-        action = random.choice(actions)  # Action aléatoire
-        state, reward, done = mdp_snake.step(action)  # Appliquer l'action et obtenir le nouvel état
-
-        print(f'Action: {action}, Reward: {reward}, State: {state}\n')
-        time.sleep(0.1)  # Pause de 0.5 secondes entre chaque tour
-        if done:
-            print("Game Over!")
-            break
+    player = GreedyPlayer(actions)
+    snake_game = ShortSnake(5)  # Supposons que cette classe existe et fonctionne comme prévu
+    
+    num_episodes = 1000
+    for episode in range(num_episodes):
+        state = snake_game.reset()
+        total_reward = 0
+        done = False
+        
+        while not done:
+            action = player.get_action(tuple(state))
+            next_state, reward, done = snake_game.step(action)
+            
+            player.update(tuple(state), action, reward, tuple(next_state))
+            
+            state = next_state
+            total_reward += reward
+        
+        if episode % 100 == 0:
+            print(f"Episode {episode}, Total Reward: {total_reward}")
 
 if __name__ == "__main__":
     main()
